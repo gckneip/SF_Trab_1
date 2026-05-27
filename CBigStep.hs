b2d9420026ffcedd30693d741d19428b925091e9
gckneip <gckneip@gmial.com>
2026-05-27 22:42:10 +0000

    Add function headers

--- CBigStep.hs
+++ CBigStep.hs
@@ -18,36 +18,48 @@ data C = While B C
    
 cbigStep :: (C,Memoria) -> (C,Memoria)
 
+-- Skip: comando vazio, não altera o estado
 cbigStep (Skip,s) = (Skip,s)
+
+-- If: escolhe um dos comandos conforme a condição booleana
 cbigStep (If b c1 c2,s)
   | bbigStep (b,s) = cbigStep (c1,s)
   | otherwise      = cbigStep (c2,s)
 
+-- Seq: executa c1 e depois c2
 cbigStep (Seq c1 c2,s) = cbigStep (c2,s')
   where (_, s') = cbigStep (c1,s)
 
-cbigStep (Atrib (Var x) e,s) = (Skip, (mudaVar s x (ebigStep (e,s))  ))  
+-- Atrib: atribui o valor da expressão à variável
+cbigStep (Atrib (Var x) e,s) = (Skip, (mudaVar s x (ebigStep (e,s))))
 
+-- While: executa c enquanto a condição b for verdadeira
 cbigStep (While b c,s)
   | bbigStep (b,s) = let (_, s') = cbigStep (c,s) in cbigStep (While b c, s')
   | otherwise = (Skip,s)
 
+-- Loop: executa c (e2 - e1) vezes
 cbigStep (Loop e1 e2 c, s)
   | ebigStep (Sub e1 e2, s) >= 0 = (Skip, s)
   | otherwise = let (_, s') = cbigStep (c, s)
                 in cbigStep (Loop (Num (ebigStep (e1, s) + 1)) e2 c, s')
 
+-- TenTimes: executa c 10 vezes (reutiliza Loop)
 cbigStep (TenTimes c, s) = cbigStep (Loop (Num 0) (Num 10) c, s)
 
+-- Repeat: executa c até b ser verdadeiro (reutiliza Seq e While)
 cbigStep (Repeat c b, s) = cbigStep (Seq c (While (Not b) c), s)
 
+-- DuplaATrib: atribuição simultânea de duas variáveis
 cbigStep (DuplaATrib (Var v1) (Var v2) e1 e2, s) =
   (Skip, mudaVar (mudaVar s v1 (ebigStep (e1, s))) v2 (ebigStep (e2, s)))
 
+-- AtribCond: atribui e1 se b for verdadeiro, e2 caso contrário
 cbigStep (AtribCond b (Var v1) e1 e2, s)
   | bbigStep (b, s) = (Skip, mudaVar s v1 (ebigStep (e1, s)))
   | otherwise       = (Skip, mudaVar s v1 (ebigStep (e2, s)))
 
+-- Swap: troca o conteúdo de duas variáveis
 cbigStep (Swap (Var x) (Var y), s) =
   (Skip, mudaVar (mudaVar s x (procuraVar s y)) y (procuraVar s x))
 
